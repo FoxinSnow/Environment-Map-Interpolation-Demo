8c57ba49dc5b692c8d22170558922015f882e8e5
FoxinSnow <huangfei037@hotmail.com>
2019-09-09 10:57:51 +0100

    initial

---
+++ textures/crop.sh
@@ -0,0 +1,13 @@
+convert JG/jg66.jpg -crop 4x3@  +repage  +adjoin  JG/cube%d.jpg
+
+convert WB/wb36.jpg -crop 4x3@  +repage  +adjoin  WB/cube%d.jpg
+
+convert GS/gs86.jpg -crop 4x3@  +repage  +adjoin  GS/cube%d.jpg
+
+convert TC/tc85.jpg -crop 4x3@  +repage  +adjoin  TC/cube%d.jpg
+
+convert CO/co35.jpg -crop 4x3@  +repage  +adjoin  CO/cube%d.jpg
+
+convert G1/g126.jpg -crop 4x3@  +repage  +adjoin  G1/cube%d.jpg
+
+convert G2/g286.jpg -crop 4x3@  +repage  +adjoin  G2/cube%d.jpg
